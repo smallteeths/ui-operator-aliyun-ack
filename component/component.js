9f0beeff8e126013eb4fdbd56f5466916e910d1e
huacheng <cheng@HuachengdeMacBook-Pro.local>
2023-07-06 11:10:02 +0800

    Update k8s version

--- component/component.js
+++ component/component.js
@@ -28,11 +28,17 @@ const defaultBase  = 1024;
 
 const languages = LANGUAGE;
 const PAGE_SIZE = 50;
+const K8S_1_26_3 = '1.26.3-aliyun.1';
 const K8S_1_24_6 = '1.24.6-aliyun.1';
 const K8S_1_22_15 = '1.22.15-aliyun.1';
-const K8S_1_20_11 = '1.20.11-aliyun.1';
 
 const VERSIONS = [
+  {
+    value:          K8S_1_26_3,
+    label:          K8S_1_26_3,
+    rancherEnabled: true,
+    aliyunEnabled:  true,
+  },
   {
     value:          K8S_1_24_6,
     label:          K8S_1_24_6,
@@ -45,14 +51,8 @@ const VERSIONS = [
     rancherEnabled: false,
     aliyunEnabled:  true,
   },
-  {
-    value:          K8S_1_20_11,
-    label:          K8S_1_20_11,
-    rancherEnabled: false,
-    aliyunEnabled:  true,
-  },
 ];
-const DEFAULT_KUBERNETES_VERSION = K8S_1_24_6;
+const DEFAULT_KUBERNETES_VERSION = K8S_1_26_3;
 const KUBERNETES = 'Kubernetes';
 const MANAGED = 'ManagedKubernetes';
 
