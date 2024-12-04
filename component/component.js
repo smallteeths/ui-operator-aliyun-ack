59a613936496bdd77367be90c2da854ec5ae2e75
smallteeths <503630985@qq.com>
2024-12-04 16:36:19 +0800

    Update k8s support version

--- component/component.js
+++ component/component.js
@@ -30,12 +30,12 @@ const languages = LANGUAGE;
 const PAGE_SIZE = 50;
 const K8S_1_26_15 = '1.26.15-aliyun.1';
 const K8S_1_24_6 = '1.24.6-aliyun.1';
-const K8S_1_28_9 = '1.28.9-aliyun.1';
+const K8S_1_28_15 = '1.28.15-aliyun.1';
 
 const VERSIONS = [
   {
-    value:          K8S_1_28_9,
-    label:          K8S_1_28_9,
+    value:          K8S_1_28_15,
+    label:          K8S_1_28_15,
     rancherEnabled: true,
     aliyunEnabled:  true,
   },
@@ -43,7 +43,7 @@ const VERSIONS = [
     value:          K8S_1_26_15,
     label:          K8S_1_26_15,
     rancherEnabled: true,
-    aliyunEnabled:  true,
+    aliyunEnabled:  false,
   },
   {
     value:          K8S_1_24_6,
@@ -52,7 +52,7 @@ const VERSIONS = [
     aliyunEnabled:  true,
   },
 ];
-const DEFAULT_KUBERNETES_VERSION = K8S_1_28_9;
+const DEFAULT_KUBERNETES_VERSION = K8S_1_28_15;
 const KUBERNETES = 'Kubernetes';
 const MANAGED = 'ManagedKubernetes';
 
